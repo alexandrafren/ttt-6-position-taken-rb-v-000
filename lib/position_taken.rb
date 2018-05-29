ff349791881b7d033106aa0bb9fb3095bb5bb904
Alexandra Fren <alexandra.fren@gmail.com>
2018-05-29 14:49:54 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 # code your #position_taken? method here!
 board = [" "," "," "," "," "," "," "," "," "]
 
-def position_taken?(board,index)
+def position_taken?(board, index)
 index_number = board - 1
   if array[index_number] == " "
     return false
