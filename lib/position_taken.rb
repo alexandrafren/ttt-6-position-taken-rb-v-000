f3f12b080f286ace5fae5105b426987c86906f13
Alexandra Fren <alexandra.fren@gmail.com>
2018-05-29 15:03:52 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -5,5 +5,9 @@ def position_taken?(board, index)
   index = index - 1
   if board[index] == " " || board[index] == "" || board[index] == nil
     return false
+  elsif board[index] == "X" || board[index] == "O"
+    return true
+  else
+    puts "error"
   end
 end
